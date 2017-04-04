b1d2145d8ff06c197b5e3c6e6c2ee55112e14a5d
colton-howe <colton.howe@uoit.net>
2017-04-03 20:09:34 -0400

    Update to Bot

--- bot.js
+++ bot.js
@@ -5,17 +5,36 @@
   Description: Initial implementation of Timing Bot
 */
 
-var JeremyDate = new Date();
-var timing = 0;
-var startTime = 0;
-var endTime = 0;
-
 // import the discord.js module
 const Discord = require('discord.js');
 
+var startTime;
+var endTime;
+var timing = 0;
+var startTime = 0;
+var endTime = 0;
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var days = 0;
+
 // create an instance of a Discord Client, and call it bot
 const client = new Discord.Client();
-const token = 'INSERT TOKEN HERE';
+const token = 'MjgxOTI2OTY5MjYyODY2NDQy.C4ifmQ.CK45dWaoCIsPGYIANTufz1ee5DU';
+
+//Utility functions
+function updateTime(){
+  endTime = new Date();
+  var timeAFK = endTime.getTime()-startTime.getTime();
+  var x = timeAFK / 1000
+  seconds = Math.floor(x % 60);
+  x /= 60
+  minutes = Math.floor(x % 60);
+  x /= 60
+  hours = Math.floor(x % 24);
+  x /= 24
+  days = Math.floor(x);
+}
 
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
@@ -28,28 +47,55 @@ client.on('message', message => {
   if (message.content === '!time') {
     if(timing == 0){
       message.channel.sendMessage('Starting Timer');
-      startTime = JeremyDate.getTime();
+      startTime = new Date();
+      timing = 1;
     } else {
-      endTime = JeremyDate.getTime();
-      var timeAFK = endTime-startTime;
-      var seconds = date.getUTCSeconds()
-      var minutes = date.getUTCMinutes()
-      var hours = date.getUTCHours()
-      var days = date.getUTCDate()-1;
+      updateTime();
       if(seconds == 0) {
-        message.channel.sendMessage('Wow, that was fast!');
+        message.channel.sendMessage("Stop spamming, Jeremy would never actually be back this fast.");
       } else if (minutes == 0) {
-        message.channel.sendMessage('Jeremy was AFK for ' + seconds + 'seconds');
+        message.channel.sendMessage('Jeremy was AFK for ' + seconds + ' seconds');
       } else if (hours == 0 && minutes < 10) {
         message.channel.sendMessage('Jeremy was AFK for ' + minutes + ' minutes and ' + seconds + ' seconds');
       } else if (hours == 0 && minutes >= 10) {
-        message.channel.sendMessage(':10minutes: Jeremy was AFK for ' + minutes + ' minutes and ' + seconds + ' seconds :10minutes:');
+        message.channel.sendMessage('<:10minutes:267176892954574848> Jeremy was AFK for ' + minutes + ' minutes and ' + seconds + ' seconds <:10minutes:267176892954574848>');
       } else if (days == 0) {
-        message.channel.sendMessage(':10minutes: Jeremy was AFK for ' + hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds :10minutes:');
+        message.channel.sendMessage('<:10minutes:267176892954574848> Jeremy was AFK for ' + hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds <:10minutes:267176892954574848>');
       } else {
-        message.channel.sendMessage(':10minutes: Jeremy was AFK for ' + days + ' days, ' + hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds :10minutes:');
+        message.channel.sendMessage('<:10minutes:267176892954574848> Jeremy was AFK for ' + days + ' days, ' + hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds <:10minutes:267176892954574848>');
+      }
+      timing = 0;
+    }
+  } else if (message.content === '!10-minutes') {
+    message.channel.sendMessage('<:10minutes:267176892954574848> <:10minutes:267176892954574848> <:BohanW:284775760277798922>    <:10minutes:267176892954574848> <:10minutes:267176892954574848> <:10minutes:267176892954574848>\n' +
+                                '<:BohanW:284775760277798922> <:10minutes:267176892954574848> <:BohanW:284775760277798922>    <:10minutes:267176892954574848> <:BohanW:284775760277798922> <:10minutes:267176892954574848>\n' + 
+                                '<:BohanW:284775760277798922> <:10minutes:267176892954574848> <:BohanW:284775760277798922>    <:10minutes:267176892954574848> <:BohanW:284775760277798922> <:10minutes:267176892954574848>\n' +
+                                '<:BohanW:284775760277798922> <:10minutes:267176892954574848> <:BohanW:284775760277798922>    <:10minutes:267176892954574848> <:BohanW:284775760277798922> <:10minutes:267176892954574848>\n' +
+                                '<:10minutes:267176892954574848> <:10minutes:267176892954574848> <:10minutes:267176892954574848>    <:10minutes:267176892954574848> <:10minutes:267176892954574848> <:10minutes:267176892954574848>\n')
+  }  else if (message.content === '!bohan') {
+      sleep(1);
+      message.channel.sendMessage('Jeremy is lying. Its a conspiracy!');
+  } else if (message.content === '!check-time') {
+    if(timing == 0){
+      message.channel.sendMessage('We aren\'t waiting on Jeremy...yet.');
+    } else {
+      updateTime();
+      if (minutes == 0) {
+        message.channel.sendMessage('Jeremy has been AFK for ' + seconds + ' seconds');
+      } else if (hours == 0 && minutes < 10) {
+        message.channel.sendMessage('Jeremy has been AFK for ' + minutes + ' minutes and ' + seconds + ' seconds');
+      } else if (hours == 0 && minutes >= 10) {
+        message.channel.sendMessage('<:10minutes:267176892954574848> Jeremy has been AFK for ' + minutes + ' minutes and ' + seconds + ' seconds <:10minutes:267176892954574848>');
+      } else if (days == 0) {
+        message.channel.sendMessage('<:10minutes:267176892954574848> Jeremy has been AFK for ' + hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds <:10minutes:267176892954574848>');
+      } else {
+        message.channel.sendMessage('<:10minutes:267176892954574848> Jeremy has been AFK for ' + days + ' days, ' + hours + ' hours, ' + minutes + ' minutes and ' + seconds + ' seconds <:10minutes:267176892954574848>');
       }
     }
+  } else if (message.content === '!commands-to-troll-jeremy') {
+      message.channel.sendMessage('__**!10-minutes**__ - <:10minutes:267176892954574848>\n' +
+                                  '__**!time**__ - Start the Jeremy AFK timer. Ends the timer if it is currently active.\n' +
+                                  '__**!check-time**__ - Check current Jeremy AFK timer.\n');
   }
 });
 
